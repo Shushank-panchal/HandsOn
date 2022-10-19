08631bf6d3003b224a9ad37fcd928d3ca439c73b
Shushank-panchal <106895994+Shushank-panchal@users.noreply.github.com>
2022-10-19 15:17:54 +0530

    Add files via upload

---
+++ ArrayString.java
@@ -0,0 +1,14 @@
+package CoreJava;
+class ArrayString
+{
+public static void main(String[] args)
+{
+String a[]	= new String[9];
+a[0]="shushank";
+a[1]="simran";
+a[6]="ram";
+a[8]="garima";
+for(int i=0;i<=a.length;i++)
+	System.out.println(a[i]);
+}
+}
